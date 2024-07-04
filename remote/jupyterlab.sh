940c89caa2fdb307659a3bedbbfac7d3ed156740
Yohaï-Eliel BERREBY <me@yberreby.com>
2024-07-04 11:50:06 -0400

    add IPYTHONDIR override to fast fs

--- remote/jupyterlab.sh
+++ remote/jupyterlab.sh
@@ -10,6 +10,7 @@ unset XDG_RUNTIME_DIR
 export JUPYTER_RUNTIME_DIR="$RUNTIME_ROOT"/jupyter-runtime
 export JUPYTER_DATA_DIR="$RUNTIME_ROOT"/jupyter-data
 export JUPYTER_CONFIG_PATH="$RUNTIME_ROOT"/jupyter-config
+export IPYTHONDIR="$RUNTIME_ROOT"/ipythondir
 mkdir -p "$JUPYTER_RUNTIME_DIR" "$JUPYTER_DATA_DIR" "$JUPYTER_CONFIG_PATH"
 
 cd
